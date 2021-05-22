9021b17a7e0b9b64f49ab4a45e216535d2ba5748
Christian Aqui <chris.aqui.ca@gmail.com>
2021-05-22 11:35:22 -0500

    Week 2 Workshop Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -66,7 +66,7 @@ campsiteRouter.route('/:campsiteId')
     Campsite.findByIdAndDelete(req.params.campsiteId)
     .then(response => {
         res.statusCode = 200;
-        res.setHeader('Conttent-Type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.json(response);
     })
     .catch(err => next(err));
